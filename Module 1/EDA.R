d5c4bbbb1da327cc86f40de7470d6cf0daa88046
jhack <jhackmeister@gmail.com>
2025-04-17 19:37:52 -0700

    updated plots in R file, added Quarto PDF report

--- Module 1/EDA.R
+++ Module 1/EDA.R
@@ -10,8 +10,19 @@ head(data)
 colSums(is.na(data)) # check for missing values
 
 # Variable distributions
-hist(carat)
+ggplot(data, aes(x=carat)) + 
+  geom_histogram(bins=10, fill = "cadetblue1", color = "black") + 
+  labs(
+    title = "Carat Distribution"
+  ) +
+  theme_minimal()
+ggplot(data, aes(x=price)) + 
+  geom_histogram(bins=10, fill = "coral", color = "black") + 
+  labs(
+    title = "Price Distribution"
+  ) +
+  theme_minimal()
 barchart(cut)
 barchart(color)
 barchart(clarity)
-hist(price)
+
